24828071bd558c5ae0015dc0a697ca721006db54
Guillem Arias Fauste <gariasfa7@alumnes.ub.edu>
2016-06-14 17:24:41 +0200

    If we go back to home within the app the page state goes back to normal

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ app.config(function($routeProvider, $locationProvider){
 app.controller('mainCtrl', ['$scope', '$sce', '$location', '$routeParams', '$rootScope', function($scope, $sce, $location, $routeParams, $rootScope){
     // RegEx filter to check valid urls. From https://gist.github.com/dperini/729294
     var reg_url = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    
     // Combine the sound and the gif. Get the data, procces it and build the mix
 	$scope.combine = function(){
 		console.log(angular.element('#gif').val().toString(), angular.element('#sound').val().toString());
@@ -29,19 +30,22 @@ app.controller('mainCtrl', ['$scope', '$sce', '$location', '$routeParams', '$roo
         $rootScope.soundModel = angular.element('#sound').val().toString(); // Get the value of the inputs
         if($rootScope.gifModel || $rootScope.soundModel){
             filterUrl($rootScope.gifModel, $rootScope.soundModel);
-             $location.path('/mix/'+encodeURIComponent($rootScope.gifModel)+'/'+encodeURIComponent($rootScope.soundModel)); // Put parameters into the url
         }else {
             console.log("no params");
         }
        
 	};
+    // This function checks that the parameter is indeed a valid url and takes action depending on result
     filterUrl = function(gif, sound){
         if((gif && gif.match(reg_url)) && (sound && sound.match(reg_url))){
             console.log("both", gif, sound);
+            $location.path('/mix/'+encodeURIComponent(gif)+'/'+encodeURIComponent(sound)); // Put parameters into the url
         }else if (gif && gif.match(reg_url)){
             console.log('just gif', gif);
+            $location.path('/mix/'+encodeURIComponent(gif)+'/ '); // Put parameters into the url
         }else if(sound && sound.match(reg_url)){
             console.log('just sound', sound);
+            $location.path('/mix/ /'+encodeURIComponent(sound)); // Put parameters into the url
         }else {
             console.log('Url Not valid');
         }
@@ -62,12 +66,28 @@ app.controller('mainCtrl', ['$scope', '$sce', '$location', '$routeParams', '$roo
                 $rootScope.gifModel = '';
                 $rootScope.soundModel = '';
             }
+            if($routeParams.title){
+                console.log("present");
+            }
     }
-
+    // We need to bring things back to the home state if we go back to the root path with no parameters
+    backHome = function(){
+        $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+          if ( $location.path() == "/" ) {
+            $scope.ngTitle = 'title';
+            $scope.ngSubtitle = 'subtitle';
+            $rootScope.gifModel = '';
+            $rootScope.soundModel = '';
+          }         
+        });
+    }
+    
+    backHome();
     checkParams();
 
 }]);
 app.controller('mixCtrl', ['$scope', '$sce', function($scope, $sce){
     $scope.src = $sce.trustAsResourceUrl('https://www.youtube-nocookie.com/embed/TbsBEb1ZxWA?rel=0&amp;showinfo=0&autoplay=1&start=20&loop=1');
     checkParams();
+    backHome();
 }]);
